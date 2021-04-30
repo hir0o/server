7d6ffb65a6ed887a19efdd16e3382e5825bce6fa
hir0o <mk.9.q.kyu@gmail.com>
2021-04-30 20:25:27 +0900

    port周りの設定

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+
 const app: express.Express = express();
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.json({
-    message: "hello ts",
+    message: "hello express",
   });
 });
 
-app.listen(3000);
+const port: number = Number(process.env.PORT) || 5000;
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
